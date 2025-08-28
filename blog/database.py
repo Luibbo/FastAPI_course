36ed1da8ecf143e00cc3255d9634f3d4edb14178
Luibbo <lubomyrbotckiv@gmail.com>
2025-08-28 17:59:28 +0300

    db in tmp

--- blog/database.py
+++ blog/database.py
@@ -3,10 +3,13 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 import os
 
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-DB_PATH = os.path.join(BASE_DIR, "blog.db")
+# BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+# DB_PATH = os.path.join(BASE_DIR, "blog.db")
 
-SQLALCHEMY_DATABASE_URL = f"sqlite:///{DB_PATH}"
+db_path = os.path.join("/tmp", "blog.db")
+SQLALCHEMY_DATABASE_URL = f"sqlite:///{db_path}"
+
+# SQLALCHEMY_DATABASE_URL = f"sqlite:///{DB_PATH}"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL, 
                        connect_args={'check_same_thread': False})
